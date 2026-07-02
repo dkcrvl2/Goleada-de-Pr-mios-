b9618ae5c27aa08692ed2cfafd9422421e5b5f19
dkcrvl2 <santosd7887@gmail.com>
2026-07-02 14:29:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-function copiarPix() {
-  navigator.clipboard.writeText("92993963533");
-  alert("✅ PIX copiado com sucesso!");
-}
-
 function enviarPalpite() {
   const nome = document.getElementById("nome").value;
-  const whats = document.getElementById("whats").value;
+  const whatsapp = document.getElementById("whatsapp").value;
   const brasil = document.getElementById("brasil").value;
   const noruega = document.getElementById("noruega").value;
-  const aviso = document.getElementById("aviso");
 
-  if (!nome || !whats || !brasil || !noruega) {
-    aviso.textContent = "⚠️ Preencha todos os campos!";
-    aviso.style.background = "#ffcccc";
-    aviso.style.color = "#a00";
+  if (!nome || !whatsapp || brasil === "" || noruega === "") {
+    alert("Preencha todos os campos!");
     return;
   }
 
-  aviso.textContent = "🎉 Palpite enviado! Boa sorte!";
-  aviso.style.background = "#ccffcc";
-  aviso.style.color = "#060";
+  alert(
+    "✅ Palpite enviado!\n\n" +
+    "Nome: " + nome +
+    "\nBrasil: " + brasil +
+    "\nNoruega: " + noruega
+  );
 }
